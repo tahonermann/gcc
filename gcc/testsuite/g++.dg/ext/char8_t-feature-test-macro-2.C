8c75e1b0871302fb625c602ed47c5c00eea369aa
Tom Honermann <tom@honermann.net>
2017-11-28 10:19:43 -0500

    char8_t: Added initial xfail tests for the __cpp_char8_t predefined feature test macro.

---
+++ gcc/testsuite/g++.dg/ext/char8_t-feature-test-macro-2.C
@@ -0,0 +1,8 @@
+// Test that predefined feature test macros are present when -fchar8_t is
+// enabled.
+// { dg-do compile }
+// { dg-options "-fchar8_t" }
+
+#if !defined(__cpp_char8_t)
+#error __cpp_char8_t is not defined! // { dg-bogus "#error __cpp_char8_t is not defined!" "char8_t" { xfail *-*-* } }
+#endif
