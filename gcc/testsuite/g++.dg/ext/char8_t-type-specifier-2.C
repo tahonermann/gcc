7cf377e2c8455f4ccf200e81fd7fa8d080804103
Tom Honermann <tom@honermann.net>
2018-01-06 22:26:11 -0500

    char8_t: Added a test to ensure the char8_t type specifier is rejected when used in combination with unsigned, signed, short, and long.

--- gcc/testsuite/g++.dg/ext/char8_t-type-specifier-2.C
+++ gcc/testsuite/g++.dg/ext/char8_t-type-specifier-2.C
@@ -3,3 +3,14 @@
 // { dg-options "-fchar8_t" }
 
 char8_t c8;
+
+signed char8_t         sc8;            /* { dg-error "signed" } */
+unsigned char8_t       uc8;            /* { dg-error "unsigned" } */
+
+short char8_t          shc8;           /* { dg-error "short" } */
+long char8_t           lgc8;           /* { dg-error "long" } */
+
+signed short char8_t   ssc8;           /* { dg-error "signed" } */
+signed long char8_t    slc8;           /* { dg-error "signed" } */
+unsigned short char8_t usc8;           /* { dg-error "unsigned" } */
+unsigned long char8_t  ulc8;           /* { dg-error "unsigned" } */
