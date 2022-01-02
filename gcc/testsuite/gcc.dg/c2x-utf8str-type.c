f4eee2bf403b62714d1ccb4542b8c85dc552a411
Tom Honermann <tom@honermann.net>
2022-01-02 00:26:17 -0500

    N2653 char8_t for C: New tests
    This change provides new tests for the core language and compiler
    dependent library changes proposed in WG14 N2653 for C.

---
+++ gcc/testsuite/gcc.dg/c2x-utf8str-type.c
@@ -0,0 +1,6 @@
+/* Test C2x UTF-8 string literal type.  */
+/* { dg-do compile } */
+/* { dg-options "-std=c2x" } */
+
+_Static_assert (_Generic (u8"text", char*: 1, unsigned char*: 2) == 2, "UTF-8 string literals have an unexpected type");
+_Static_assert (_Generic (u8"x"[0], char:  1, unsigned char:  2) == 2, "UTF-8 string literal elements have an unexpected type");
---
+++ gcc/testsuite/gcc.dg/gnu2x-predefined-macros.c
@@ -0,0 +1,5 @@
+/* Test C2x predefined macros with -std=gnu2x.  */
+/* { dg-do compile } */
+/* { dg-options "-std=c2x" } */
+
+#include "c2x-predefined-macros.c"
